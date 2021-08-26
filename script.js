238b8901d51a2ba26837c446d7c078c4a1c0c3c6
voncardoso <vonharrison26@gmail.com>
2021-08-26 12:30:35 -0300

    animação da função cor

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const tabTamanho = document.querySelectorAll('.tamanho a');
-tabTamanho[0].classList.add('ativo');
+const tabCor = document.querySelectorAll('.corRoupa a');
+tabTamanho[1].classList.add('ativo');
+tabCor[1].classList.add('ativo');
 
 function activeTab(index){
 
@@ -9,8 +11,22 @@ function activeTab(index){
     tabTamanho[index].classList.add('ativo');
 }
 
+function activeTabCor(index){
+
+    tabCor.forEach((item) => {
+        item.classList.remove('ativo')
+    });
+    tabCor[index].classList.add('ativo');
+}
+
 tabTamanho.forEach((intem, index) =>{
     intem.addEventListener('click', () => {
         activeTab(index);
     });
 });
+
+tabCor.forEach((intem, index)=>{
+    intem.addEventListener('click', ()=>{
+        activeTabCor(index);
+    })
+})
